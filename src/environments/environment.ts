94a0a142e3a5e073e131c86880ceacfe163b8b9c
Madhawa Weerasinghe <madhawazu@gmail.com>
2022-11-29 07:59:23 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,6 @@ export const environment = {
   weatherApiUrl: "https://cors-anywhere.herokuapp.com/https://api.openweathermap.org",
   locationApiUrl: "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/",
 
-  weatherApiKey: "a1ea7934a81e396db1cc6106f6691987",
-  locationApiKey: "AIzaSyCjlcfim7QiuSWVaFOWUirGEiDBvdb1WqU"
+  weatherApiKey: "",
+  locationApiKey: ""
 };
